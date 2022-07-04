1fc37bd5bbcdab0a1272816ad30ca9f9c20ed68a
nachomat85 <ignaciomateu@gmail.com>
2022-07-04 20:25:10 -0300

    estilos y efectos

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express()
 const path = require('path')
 const publicPath = path.resolve(__dirname,'./public')
+// const viewsPath = path.resolve(__dirname,'./views')
+
 app.use(express.static(publicPath))
+// app.use(express.static(viewsPath))
 
 
 app.listen(3000,() => {
@@ -11,4 +14,12 @@ app.listen(3000,() => {
 
 app.get('/', (req, res)=> {
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
+
+app.get('/register/', (req, res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))
+})
+
+app.get('/login/', (req, res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
 })
